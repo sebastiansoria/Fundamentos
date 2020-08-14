a5103e73ec984b25fe7d810a5c72165b44d2f7a0
sebastiansoria <69353079+sebastiansoria@users.noreply.github.com>
2020-08-14 09:40:47 -0400

    Add files via upload
    SUMA DE 2 NUMEROS C++

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,x ;
+    
+ cout <<"Dame 2 numeros y los sumo"<<endl;
+   cout <<"Escribe el primer sumando"<<endl;
+cin >>a;
+  cout <<"Escribe el segundo sumando";
+cin >>b;
+x=a+b ;
+cout <<"Esta es la suma capo"<< x;
+    return 0;
+}
