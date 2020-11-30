daedf3c3642a241ccde6640bb964806891e5044c
sebastiansoria <69353079+sebastiansoria@users.noreply.github.com>
2020-11-30 16:49:13 -0400

    Add files via upload

---
+++ Cambio de variables funciones.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include"conio.h"
+#include"math.h"
+using namespace std;
+float CambiodeVariables(float r, float a);
+int main()
+{
+float x,y;
+cout<<"Ingrese el valor de la variable x";
+cin>>x;
+cout<<"Ingrese el valor de la variable y";
+cin>>y;
+x = CambiodeVariables(x,y);//En la llamada no se colocan ,los tipos de dato
+cout<<"x = "<<y;
+}
+
+
+ float  CambiodeVariables(float r, float a)
+ {
+ 	float y;
+ 	y=x;
+ 	
+ }
